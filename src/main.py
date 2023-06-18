6eacb8a782539a8e6aafda26dfb996c63e2e7911
kitoketa <rooney2406@gmail.com>
2023-06-18 16:50:12 +0900

    test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from os import environ
-from azure.cli.core import get_default_cli
+import subprocess
+from subprocess import PIPE
 
 def main():
     name = environ.get('INPUT_NAME')
@@ -10,48 +11,46 @@ def main():
     control_plane_only = environ.get('INPUT_CONTROL_PLANE_ONLY', False)
     no_wait = environ.get('INPUT_NO_WAIT', False)
     node_image_only = environ.get('INPUT_NODE_IMAGE_ONLY', False)
-    
-    cli = get_default_cli()
 
     ACCOUNT_LIST = [
         'account',
-        'list',
-        '--subscription', '1e6bd428-8625-4f07-bb44-f3f6a2dd5315'
+        'list'
     ]
     print('run account list')
-    cli.invoke(ACCOUNT_LIST)
+    result = subprocess.run(ACCOUNT_LIST, stdout=PIPE, stderr=PIPE, text=True)
+    print(result.stdout)
 
-    ACCOUNT_SET = [
-        'account',
-        'set',
-        '--subscription', '1e6bd428-8625-4f07-bb44-f3f6a2dd5315'
-    ]
-    print('run account set')
-    cli.invoke(ACCOUNT_SET)
+    # ACCOUNT_SET = [
+    #     'account',
+    #     'set',
+    #     '--subscription', '1e6bd428-8625-4f07-bb44-f3f6a2dd5315'
+    # ]
+    # print('run account set')
+    # cli.invoke(ACCOUNT_SET)
 
-    AKS_GET_CREDENTIALS = [
-        'aks',
-        'get-credentials',
-        '--resource-group', resource_group,
-        '--name', name
-    ]
-    print('run aks get-credentials')
-    cli.invoke(AKS_GET_CREDENTIALS)
+    # AKS_GET_CREDENTIALS = [
+    #     'aks',
+    #     'get-credentials',
+    #     '--resource-group', resource_group,
+    #     '--name', name
+    # ]
+    # print('run aks get-credentials')
+    # cli.invoke(AKS_GET_CREDENTIALS)
 
-    AKS_UPGRADE = [
-        'aks',
-        'upgrade',
-        '--name', name,
-        '--subscription', '1e6bd428-8625-4f07-bb44-f3f6a2dd5315',
-        '--resource-group', resource_group,
-        '--kubernetes-version', kubernetes_version,
-        # '--control_plane_only',
-        # '--no_wait',
-        # '--node_image_only',
-        '--yes'
-    ]
-    print('run aks upgrade')
-    cli.invoke(AKS_UPGRADE)
+    # AKS_UPGRADE = [
+    #     'aks',
+    #     'upgrade',
+    #     '--name', name,
+    #     '--subscription', '1e6bd428-8625-4f07-bb44-f3f6a2dd5315',
+    #     '--resource-group', resource_group,
+    #     '--kubernetes-version', kubernetes_version,
+    #     # '--control_plane_only',
+    #     # '--no_wait',
+    #     # '--node_image_only',
+    #     '--yes'
+    # ]
+    # print('run aks upgrade')
+    # cli.invoke(AKS_UPGRADE)
 
     # TODO エラー処理
 
